f6f82c4082c306e79f3a3a4142133383986ccdfb
Venuvk0805 <rvenukanth5@gmail.com>
2025-06-18 12:47:26 +0530

    installed expresss instead of express so sorted that

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,10 @@
 const express = require ("express") ;
 const app =express();
 
+app.get("/",(req,res)=>{
+    res.send("Hello Venu , welcome to express js ")
+})
+
 app.listen("4000",()=>{
     console.log("listening to port 4000")
 })
