cefe669c923124f98b7c8c419445068fc54f3a1d
Venuvk0805 <rvenukanth5@gmail.com>
2025-06-19 10:53:24 +0530

    created Task_model and taskSchema

---
+++ Backend/Models/Task_model.js
@@ -0,0 +1,16 @@
+const mongoose=require("mongoose");
+
+const Schema=mongoose.Schema;
+
+const taskSchema=new Schema(
+    {
+        title:{
+            type:String,
+            require:true
+        },
+        description:{
+            type:String
+        }
+    },
+    {timestamps:true}
+);
