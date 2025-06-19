0a61f7cd08fb7e2bdb5c84a5f4a9fc2561850331
Venuvk0805 <rvenukanth5@gmail.com>
2025-06-19 09:09:46 +0530

    Connected DB

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,27 @@
 const express = require ("express") ;
+require('dotenv').config();
+// Using Node.js `require()`
+const mongoose = require('mongoose');
+
 const app =express();
+
+app.use((req,res,next)=>{
+    console.log("Path is " +req.path + "method is " +req.method);
+    next();
+});
 
 app.get("/",(req,res)=>{
     res.send("Hello Venu , welcome to express js ")
-})
+});
 
-app.listen("4000",()=>{
-    console.log("listening to port 4000")
-})
+mongoose
+.connect(process.env.MONGO_URI)
+    .then(()=>{
+        app.listen(process.env.PORT,()=>{
+        console.log("MongoDB connected successfully and listening to port "+ process.env.PORT)
+        })
+    })
+    .catch((error)=>console.log(error));
+
+
+
